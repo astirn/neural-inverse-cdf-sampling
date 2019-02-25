5e6e886214cc9966b90eb55de316315d1f9854a3
astirn <andrew.stirn@gmail.com>
2019-02-25 09:15:31 -0500

    random seeds and import errors resolved for AEVITM.

--- autoencoding_vi_for_topic_models/models/__init__.py
+++ autoencoding_vi_for_topic_models/models/__init__.py
@@ -1,2 +1,2 @@
-from prodlda import VAE
-from nvlda import VAE
+from autoencoding_vi_for_topic_models.models.prodlda import VAE
+from autoencoding_vi_for_topic_models.models.nvlda import VAE
--- autoencoding_vi_for_topic_models/run.py
+++ autoencoding_vi_for_topic_models/run.py
@@ -10,26 +10,26 @@ from time import time
 import matplotlib.pyplot as plt
 import pickle
 import sys, getopt
-from models import prodlda, nvlda
+from autoencoding_vi_for_topic_models.models import prodlda, nvlda
 '''-----------Data--------------'''
 def onehot(data, min_length):
     return np.bincount(data, minlength=min_length)
 
 dataset_tr = 'data/20news_clean/train.txt.npy'
-data_tr = np.load(dataset_tr)
+data_tr = np.load(dataset_tr, encoding='bytes')
 dataset_te = 'data/20news_clean/test.txt.npy'
-data_te = np.load(dataset_te)
+data_te = np.load(dataset_te, encoding='bytes')
 vocab = 'data/20news_clean/vocab.pkl'
-vocab = pickle.load(open(vocab,'r'))
+vocab = pickle.load(open(vocab,'rb'))
 vocab_size=len(vocab)
 #--------------convert to one-hot representation------------------
-print 'Converting data to one-hot representation'
+print('Converting data to one-hot representation')
 data_tr = np.array([onehot(doc.astype('int'),vocab_size) for doc in data_tr if np.sum(doc)!=0])
 data_te = np.array([onehot(doc.astype('int'),vocab_size) for doc in data_te if np.sum(doc)!=0])
 #--------------print the data dimentions--------------------------
-print 'Data Loaded'
-print 'Dim Training Data',data_tr.shape
-print 'Dim Test Data',data_te.shape
+print('Data Loaded')
+print('Dim Training Data',data_tr.shape)
+print('Dim Test Data',data_te.shape)
 '''-----------------------------'''
 
 '''--------------Global Params---------------'''
@@ -66,11 +66,9 @@ def make_network(layer1=100,layer2=100,num_topics=50,bs=200,eta=0.002):
 
 '''--------------Methods--------------'''
 def create_minibatch(data):
-    rng = np.random.RandomState(10)
-
     while True:
         # Return random data samples of a size 'minibatch_size' at each iteration
-        ixs = rng.randint(data.shape[0], size=batch_size)
+        ixs = np.random.randint(data.shape[0], size=batch_size)
         yield data[ixs]
 
 
@@ -93,30 +91,30 @@ def train(network_architecture, minibatches, type='prodlda',learning_rate=0.001,
         total_batch = int(n_samples_tr / batch_size)
         # Loop over all batches
         for i in range(total_batch):
-            batch_xs = minibatches.next()
+            batch_xs = next(minibatches)
             # Fit training using batch data
             cost,emb = vae.partial_fit(batch_xs)
             # Compute average loss
             avg_cost += cost / n_samples_tr * batch_size
 
             if np.isnan(avg_cost):
-                print epoch,i,np.sum(batch_xs,1).astype(np.int),batch_xs.shape
-                print 'Encountered NaN, stopping training. Please check the learning_rate settings and the momentum.'
+                print(epoch,i,np.sum(batch_xs,1).astype(np.int),batch_xs.shape)
+                print('Encountered NaN, stopping training. Please check the learning_rate settings and the momentum.')
                 # return vae,emb
                 sys.exit()
 
         # Display logs per epoch step
         if epoch % display_step == 0:
-            print "Epoch:", '%04d' % (epoch+1), \
-                  "cost=", "{:.9f}".format(avg_cost)
+            print("Epoch:", '%04d' % (epoch+1), \
+                  "cost=", "{:.9f}".format(avg_cost))
     return vae,emb
 
 def print_top_words(beta, feature_names, n_top_words=10):
-    print '---------------Printing the Topics------------------'
+    print('---------------Printing the Topics------------------')
     for i in range(len(beta)):
         print(" ".join([feature_names[j]
             for j in beta[i].argsort()[:-n_top_words - 1:-1]]))
-    print '---------------End of Topics------------------'
+    print('---------------End of Topics------------------')
 
 def calcPerp(model):
     cost=[]
@@ -125,7 +123,7 @@ def calcPerp(model):
         n_d = np.sum(doc)
         c=model.test(doc)
         cost.append(c/n_d)
-    print 'The approximated perplexity is: ',(np.exp(np.mean(np.array(cost))))
+    print('The approximated perplexity is: ',(np.exp(np.mean(np.array(cost)))))
 
 def main(argv):
     m = ''
@@ -138,15 +136,15 @@ def main(argv):
     try:
       opts, args = getopt.getopt(argv,"hpnm:f:s:t:b:r:,e:",["default=","model=","layer1=","layer2=","num_topics=","batch_size=","learning_rate=","training_epochs"])
     except getopt.GetoptError:
-        print 'CUDA_VISIBLE_DEVICES=0 python run.py -m <model> -f <#units> -s <#units> -t <#topics> -b <batch_size> -r <learning_rate [0,1] -e <training_epochs>'
+        print('CUDA_VISIBLE_DEVICES=0 python run.py -m <model> -f <#units> -s <#units> -t <#topics> -b <batch_size> -r <learning_rate [0,1] -e <training_epochs>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'CUDA_VISIBLE_DEVICES=0 python run.py -m <model> -f <#units> -s <#units> -t <#topics> -b <batch_size> -r <learning_rate [0,1]> -e <training_epochs>'
+            print('CUDA_VISIBLE_DEVICES=0 python run.py -m <model> -f <#units> -s <#units> -t <#topics> -b <batch_size> -r <learning_rate [0,1]> -e <training_epochs>')
             sys.exit()
         elif opt == '-p':
-            print 'Running with the Default settings for prodLDA...'
-            print 'CUDA_VISIBLE_DEVICES=0 python run.py -m prodlda -f 100 -s 100 -t 50 -b 200 -r 0.002 -e 100'
+            print('Running with the Default settings for prodLDA...')
+            print('CUDA_VISIBLE_DEVICES=0 python run.py -m prodlda -f 100 -s 100 -t 50 -b 200 -r 0.002 -e 100')
             m='prodlda'
             f=100
             s=100
@@ -155,8 +153,8 @@ def main(argv):
             r=0.002
             e=100
         elif opt == '-n':
-            print 'Running with the Default settings for NVLDA...'
-            print 'CUDA_VISIBLE_DEVICES=0 python run.py -m nvlda -f 100 -s 100 -t 50 -b 200 -r 0.005 -e 300'
+            print('Running with the Default settings for NVLDA...')
+            print('CUDA_VISIBLE_DEVICES=0 python run.py -m nvlda -f 100 -s 100 -t 50 -b 200 -r 0.005 -e 300')
             m='nvlda'
             f=100
             s=100
@@ -181,11 +179,17 @@ def main(argv):
 
     minibatches = create_minibatch(docs_tr.astype('float32'))
     network_architecture,batch_size,learning_rate=make_network(f,s,t,b,r)
-    print network_architecture
-    print opts
+    print(network_architecture)
+    print(opts)
     vae,emb = train(network_architecture, minibatches,m, training_epochs=e,batch_size=batch_size,learning_rate=learning_rate)
-    print_top_words(emb, zip(*sorted(vocab.items(), key=lambda x: x[1]))[0])
+    # print_top_words(emb, zip(*sorted(vocab.items(), key=lambda x: x[1]))[0])
     calcPerp(vae)
 
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+
+    # set random seeds
+    np.random.seed(123)
+    tf.set_random_seed(123)
+
+    main(sys.argv[1:])
